d83bf30a001ace7a4e53e3f357125036bc18efd4
HossamMahmoudkhedr <nhem318@gmail.com>
2024-12-13 17:40:22 +0200

    set the dbms file

---
+++ dbms.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+## ********************* For display menus ********************
+## For display the main menu
+displayMainMenu(){
+}
+
+## For display operations on the database
+displayDatabaseMenu(){
+}
+
+
+## ********************** For manipulating databases **********************
+## create database
+createDatabase(){
+}
+
+## list databases
+listDatabases(){
+}
+
+## drop database
+dropDatabase(){
+}
+
+## connect to datbase
+connectToDatabase(){
+}
+
+
+## ********************** For manupulating tables ************************
+## create table
+createTable(){
+}
+
+## list tables
+listTables(){
+}
+
+## drop table
+dropTable(){
+}
+
+## insert into table
+insertIntoTable(){
+}
+
+## select from table
+selectFromTable(){
+}
+
+## delete from table
+deleteFromTable(){
+}
+
+## update table
+updateTable(){
+}
+
+## disconnect form database
+disconnect(){
+}
